a0b749f92bdfd0d4c8cad4e3aefc0a3ca8224740
fatihege <fatihxege@gmail.com>
2021-09-05 14:09:04 +0300

    Added volume command

---
+++ src/commands/music/Volume.ts
@@ -0,0 +1,101 @@
+import wio from 'wio.db';
+import { MessageEmbed, TextChannel, VoiceChannel } from 'discord.js';
+import { ExecuteFunction } from '../../interfaces/Command';
+import { Emoji } from '../../Constants';
+import { getConnection, getStreamDispatcher } from '../../utility/VoiceConnection';
+
+export const aliases: string[] = ['volume', 'vol', 'ses'];
+export const description: string = 'command.volume.description';
+export const category: string = 'category.music';
+export const usage: string = 'command.volume.usage';
+export const examples: string[] = ['50', '200', '0'];
+export const maxArgs: number = 1;
+export const botPermissions: string[] = ['CONNECT', 'SPEAK'];
+export const execute: ExecuteFunction = async (client, server, message, args, colors) => {
+    let embed: MessageEmbed;
+    const voiceChannel: VoiceChannel = message.member.voice.channel;
+    let serverQueue = await wio.fetch(`queue_${message.guild.id}`);
+
+    if (!serverQueue || !serverQueue.songs.length || !getConnection(message.guild.id)) {
+        embed = client.embed({
+            color: colors.RED,
+            description: 'Bu sunucuya ait bir oynatma listesi bulunamadı.'
+        });
+
+        return client.tempMessage(message.channel as TextChannel, embed, 10000);
+    }
+
+    if (!voiceChannel) {
+        embed = client.embed({
+            color: colors.RED,
+            description: 'Lütfen bir ses kanalına bağlanın.'
+        });
+
+        return client.tempMessage(message.channel as TextChannel, embed, 10000);
+    }
+
+    if (voiceChannel.id != serverQueue.voiceChannel) {
+        embed = client.embed({
+            color: colors.RED,
+            description: 'Sizin bulunduğunuz kanalda müzik oynatılmıyor.'
+        });
+
+        return client.tempMessage(message.channel as TextChannel, embed, 10000);
+    }
+
+    serverQueue.textChannel = message.channel.id;
+    await wio.set(`queue_${message.guild.id}`, serverQueue);
+
+    const streamDispatcher = getStreamDispatcher(message.guild.id);
+
+    if (!args[0]) {
+        embed = client.embed({
+            color: colors.BLUE,
+            description: `Müziğin ses seviyesi: **%${streamDispatcher.volume * 100}**`
+        });
+
+        return message.channel.send(embed);
+    }
+
+    const volume = parseInt(args[0]);
+
+    if (isNaN(volume)) {
+        embed = client.embed({
+            color: colors.RED,
+            description: 'Lütfen bir sayı girin.'
+        });
+
+        return client.tempMessage(message.channel as TextChannel, embed, 10000);
+    }
+
+    if (volume < 0 || volume > 200) {
+        embed = client.embed({
+            color: colors.RED,
+            description: 'Ses seviyesi 0-200 aralığında olmalıdır.'
+        });
+
+        return client.tempMessage(message.channel as TextChannel, embed, 10000);
+    }
+
+    try {
+        streamDispatcher.setVolume(volume / 100);
+
+        embed = client.embed({
+            color: colors.BLUE,
+            description: `Ses seviyesi **%${volume}** olarak ayarlandı.`
+        });
+
+        message.channel.send(embed);
+    } catch (e) {
+        embed = client.embed({
+            color: colors.RED,
+            description: `Ses seviyesi ayarlanırken bir sorun oluştu.`
+        });
+
+        return client.tempMessage(message.channel as TextChannel, embed, 10000);
+    }
+
+    try {
+        await message.react(Emoji.THUMBSUP);
+    } catch (e) {}
+}
